1031d22cfb23c2ac2804163a5334dd4ed06895ed
Isaac Messer <isaac.messer640.com>
2023-04-25 21:39:07 -0600

    Included a <BudgetProvider> element wrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './Pages/Home';
 import { Settings } from './Pages/Settings';
 import { NavBar } from './Pages/Components/NavBar';
+import { BudgetProvider } from './Pages/Context';
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='settings' element={<Settings />} />
-      </Routes>
+      <BudgetProvider>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='settings' element={<Settings />} />
+        </Routes>
+      </BudgetProvider>
+      
     </>
     
   );
