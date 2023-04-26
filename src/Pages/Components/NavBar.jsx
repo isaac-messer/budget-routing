7c9cceebe7e103e573b8769bf680b458a51a97dd
Isaac Messer <isaac.messer640.com>
2023-04-25 21:41:14 -0600

    Added div(s) to display user input info in nav

--- src/Pages/Components/NavBar.jsx
+++ src/Pages/Components/NavBar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { BudgetContext } from '../Context';
+
 export const NavBar = () => {
+    const { user, title } = useContext(BudgetContext);
+
     return (
-        <>
+        <>    
             <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/settings'>Settings</Link>
-            </nav>
+                <div id="personalInfo">
+                    <h4>{user}</h4>       
+                </div>
+                <div id="titleInfo">
+                    <h3>{title}</h3>
+                </div>
+                <div id="navInfo">
+                    <h4><span>
+                        <Link to='/'>Home</Link>
+                        <Link to='/settings'>Settings</Link>   
+                    </span></h4>
+                </div>
+                
+            </nav> 
         </>
-    )
-}
+    );
+};
